7d842819806750fd2088424d5ad2a02bc0d5259b
VANAPALLI GNANESWARA RAO <112818596+Gnaneswar2004@users.noreply.github.com>
2025-07-14 08:18:47 +0530

    Added Assignments

--- JAVA Assignment 1/Frequency_of_Elements_in_Array.java
+++ JAVA Assignment 1/Frequency_of_Elements_in_Array.java
@@ -12,23 +12,23 @@ public class Frequency_of_Elements_in_Array {
 		}
 		Count_Frequency(array,len);
 	}
-	public static void Count_Frequency(int arr[], int n)
+    public static void Count_Frequency(int arr[], int n)
 	{
 		boolean visited[] = new boolean [n];
 		for (int i=0; i<n; i++) 
 		{
-        if (visited[i] == true)
-            continue;
-		int count = 1;
-        for (int j=i+1; j<n; j++) 
-		{
-            if (arr[i] == arr[j]) 
+        		if (visited[i] == true)
+            		continue;
+			int count = 1;
+        		for (int j=i+1; j<n; j++) 
 			{
-                visited[j] = true;
-                count++;
-            }
-        }
-        System.out.println(arr[i] + " " + count);
+           			if (arr[i] == arr[j]) 
+				{
+                		visited[j] = true;
+                		count++;
+            			}
+        		}
+        	System.out.println(arr[i] + " " + count);
 		}
-    }
+	}
 }
