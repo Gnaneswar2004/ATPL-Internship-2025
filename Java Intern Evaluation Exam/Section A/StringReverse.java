dd7437a6200133c8db85885026891a0073911fe7
VANAPALLI GNANESWARA RAO <112818596+Gnaneswar2004@users.noreply.github.com>
2025-07-16 08:08:29 +0530

    Section A - Question - 2.

--- Java Intern Evaluation Exam/Section A/StringReverse.java
+++ Java Intern Evaluation Exam/Section A/StringReverse.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class StringReverse {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        int len = str.length();
-        char[] str1 = str.toCharArray();
+        String string = scanner.nextLine();
+        int len = string.length();
+        char[] str = string.toCharArray();
         for (int i=0; i<len; i++) {
-            System.out.print(str1[len-1-i]);
+            System.out.print(str[len-1-i]);
         }
     }
 }
