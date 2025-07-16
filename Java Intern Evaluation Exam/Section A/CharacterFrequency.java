dbb9e564fc437c3e2a9f77b7c0136590fb3c994b
Gnaneswara rao <gnani07012004@gmail.com>
2025-07-16 08:03:20 +0530

    Added Java Intern Evaluation Exam

---
+++ Java Intern Evaluation Exam/Section A/CharacterFrequency.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class CharacterFrequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int len = str.length();
+        char[] characters = str.toCharArray();
+        Count_Frequency(characters,len);
+    }
+    public static void Count_Frequency(char arr[], int n)
+    {
+        boolean visited[] = new boolean [n];
+        for (int i=0; i<n; i++)
+        {
+            if (visited[i])
+                continue;
+            int count = 1;
+            for (int j=i+1; j<n; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " " + count);
+        }
+    }
+}
