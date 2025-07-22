7cc5afa5e2db09e5132081ebfb2c593a6ff50dce
Gnaneswara rao <gnani07012004@gmail.com>
2025-07-22 11:03:07 +0530

    Added SQL Assignments

---
+++ SQL Assignment - Dashboard Data Queries/Basic Business Metrics.sql
@@ -0,0 +1,10 @@
+-- 1. What is the total number of orders?
+
+select count(OrderID) from Orders;
+
+-- 2. How many customers are there?
+
+select count(CustomerID) from Customers;
+
+-- 3. What is the total revenue from all orders?
+
