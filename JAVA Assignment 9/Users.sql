5334df3f1fea94e3a4e937471eb949b709117639
Gnaneswara rao <gnani07012004@gmail.com>
2025-07-22 16:10:56 +0530

    Added Assignments

--- JAVA Assignment 9/Users.sql
+++ JAVA Assignment 9/Users.sql
@@ -1,10 +1,11 @@
 create table users_gnani (
 	user_id int primary key,
     user_name varchar(50),
-    user_email varchar(50)
+    user_email varchar(50),
+    dob date
 );
 
-insert into users_gnani (user_id, user_name, user_email) values
-(101, 'Gnani', 'gnani@gmail.com'), (102, 'Sai', 'sai@gmail.com'), (103, 'Prasanth', 'prasanth@gmail.com');
+insert into users_gnani (user_id, user_name, user_email, dob) values
+(101, 'Gnani', 'gnani@gmail.com', '2004-01-07'), (102, 'Sai', 'sai@gmail.com', '2003-02-10'), (103, 'Prasanth', 'prasanth@gmail.com', '2004-05-24');
 
 select * from users_gnani;
