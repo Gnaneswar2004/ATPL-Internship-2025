a8f79d530c10a8bcefd66636531354e20f836836
VANAPALLI GNANESWARA RAO <112818596+Gnaneswar2004@users.noreply.github.com>
2025-07-22 22:53:38 +0530

    Added SQL Assignments

--- SQL Assignment - Dashboard Data Queries/Basic Business Metrics.sql
+++ SQL Assignment - Dashboard Data Queries/Basic Business Metrics.sql
@@ -1,10 +1,18 @@
+-- Basic Business Metrics
+
 -- 1. What is the total number of orders?
 
-select count(OrderID) from Orders;
+SELECT COUNT(*) AS TotalOrders FROM Orders;
 
 -- 2. How many customers are there?
 
-select count(CustomerID) from Customers;
+SELECT COUNT(*) AS TotalCustomers FROM Customers;
 
 -- 3. What is the total revenue from all orders?
 
+SELECT 
+    SUM(od.Quantity * p.Price) AS TotalRevenue
+FROM 
+    OrderDetails od
+JOIN 
+    Products p ON od.ProductID = p.ProductID;
